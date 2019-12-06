681de123145ea1e140581e32d136a7c9c72dcf32
Llyme <InfiniteX777@gmail.com>
2019-12-07 06:06:02 +0800

    Initial commit

---
+++ Plugin/Plugin.GUI.cs
@@ -0,0 +1,407 @@
+﻿using AIProject;
+using AIProject.Definitions;
+using CharaCustom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using Sickness = AIProject.SaveData.Sickness;
+
+namespace AgentTrainer
+{
+	public partial class AgentTrainer
+	{
+		const string HEADER = "Agent Trainer";
+
+		const float MARGIN_TOP = 20f;
+		const float MARGIN_BOTTOM = 10f;
+		const float MARGIN_LEFT = 10f;
+		const float MARGIN_RIGHT = 10f;
+		const float WIDTH = 540f;
+		const float HEIGHT = 460f;
+		const float INNER_WIDTH = WIDTH - MARGIN_LEFT - MARGIN_RIGHT;
+		const float INNER_HEIGHT = HEIGHT - MARGIN_TOP - MARGIN_BOTTOM;
+
+		const float ENTRY_WIDTH = 200f;
+		const float AGENTS_WIDTH = 120f;
+		const float LABEL_WIDTH = 90f;
+		const float VALUE_WIDTH = 40f;
+
+		static Rect rect = new Rect(
+			Screen.width - WIDTH,
+			(Screen.height - HEIGHT) / 2,
+			WIDTH,
+			HEIGHT
+		);
+		static Rect innerRect = new Rect(
+			MARGIN_LEFT,
+			MARGIN_TOP,
+			INNER_WIDTH,
+			INNER_HEIGHT
+		);
+		static Rect dragRect = new Rect(0f, 0f, WIDTH, 20f);
+
+		GUIStyle labelStyle;
+		GUIStyle selectedButtonStyle;
+
+		readonly Dictionary<Desire.Type, int> desireTable = typeof(Desire).GetField(
+			"_desireKeyTable",
+			BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic
+		).GetValue(null) as Dictionary<Desire.Type, int>;
+
+		readonly List<string> tabs = new List<string>()
+		{
+			"Info",
+			"Sliders"
+		};
+		int tab = 0;
+
+		bool visible = false;
+
+		void OnGUI()
+		{
+			if (!visible)
+				return;
+
+			if (labelStyle == null)
+			{
+				labelStyle = new GUIStyle(GUI.skin.label)
+				{
+					fontStyle = FontStyle.Bold
+				};
+
+				selectedButtonStyle = new GUIStyle(GUI.skin.button)
+				{
+					fontStyle = FontStyle.Bold,
+					normal = {
+						textColor = Color.red
+					},
+					focused = {
+						textColor = Color.red
+					},
+					active = {
+						textColor = Color.red
+					},
+					hover = {
+						textColor = Color.red
+					},
+				};
+			}
+
+			rect = GUI.Window(
+				WindowID.Value,
+				rect,
+				Draw,
+				HEADER
+			);
+		}
+
+		float Draw_Contents_Slider(string label,
+								   bool locked,
+								   float value,
+								   float min,
+								   float max,
+								   out bool toggle)
+		{
+			GUILayout.BeginHorizontal();
+			{
+				toggle = GUILayout.Button(
+					label,
+					locked ? selectedButtonStyle : GUI.skin.button,
+					GUILayout.Width(LABEL_WIDTH)
+				);
+				value = GUILayout.HorizontalSlider(value, min, max);
+
+				GUILayout.Label($"{value:F0}", GUILayout.Width(VALUE_WIDTH));
+			}
+			GUILayout.EndHorizontal();
+
+			return value;
+		}
+
+		float Draw_Contents_Slider(Dictionary<int, float> dict,
+								   int key,
+								   string label,
+								   float value,
+								   float min,
+								   float max)
+		{
+			bool locked = dict.ContainsKey(key);
+
+			if (locked)
+				value = dict[key];
+
+			float next = Draw_Contents_Slider(
+				label,
+				locked,
+				value,
+				min,
+				max,
+				out bool toggle
+			);
+
+			if (toggle)
+				if (locked)
+					dict.Remove(key);
+				else
+					dict[key] = next;
+			else if (locked)
+				dict[key] = next;
+
+			return next;
+		}
+
+		int Draw_Contents_Slider(Dictionary<int, int> dict,
+								 int key,
+								 string label,
+								 int value,
+								 float min,
+								 float max)
+		{
+			int next = (int)Draw_Contents_Slider(
+				label,
+				dict.ContainsKey(key),
+				value,
+				min,
+				max,
+				out bool toggle
+			);
+
+			if (toggle)
+				if (dict.ContainsKey(key))
+					dict.Remove(key);
+				else
+					dict[key] = next;
+			else if (dict.ContainsKey(key))
+				dict[key] = next;
+
+			return next;
+		}
+
+		void Draw_Sliders_Stats()
+		{
+			if (controller.agent == null)
+				return;
+
+			GUILayout.Label("Stats", labelStyle);
+
+			Dictionary<int, float> stats = controller.agent.AgentData.StatsTable;
+
+			foreach (int stat in stats.Keys.ToList())
+			{
+				float curr = stats[stat];
+				float next = Draw_Contents_Slider(
+					controller.lockedStats,
+					stat,
+					((Status.Type)stat).ToString(),
+					curr,
+					0f,
+					100f
+				);
+
+				if (next != curr)
+					stats[stat] = next;
+			}
+		}
+
+		void Draw_Sliders_Flavors()
+		{
+			GUILayout.Label("Flavors", labelStyle);
+
+			Dictionary<int, int> flavors = controller.ChaControl.fileGameInfo.flavorState;
+
+			foreach (int flavor in flavors.Keys.ToList())
+			{
+				int curr = flavors[flavor];
+				int next = Draw_Contents_Slider(
+					controller.lockedFlavors,
+					flavor,
+					((FlavorSkill.Type)flavor).ToString(),
+					curr,
+					0f,
+					9999f
+				);
+
+				if (next != curr)
+					if (controller.agent != null)
+						controller.agent.SetFlavorSkill(flavor, next);
+					else
+						SetFlavorSkill(controller.ChaControl.fileGameInfo, flavor, next);
+			}
+		}
+
+		void Draw_Sliders_Desires()
+		{
+			if (controller.agent == null)
+				return;
+
+			GUILayout.BeginVertical(GUILayout.Width(ENTRY_WIDTH));
+			{
+				GUILayout.Label("Desires", labelStyle);
+
+				Dictionary<int, float> desires = controller.agent.AgentData.DesireTable;
+
+				foreach (KeyValuePair<Desire.Type, int> desire in desireTable)
+				{
+					int key = desire.Value;
+					float curr = desires[key];
+					float next = Draw_Contents_Slider(
+						controller.lockedDesires,
+						key,
+						desire.Key.ToString(),
+						curr,
+						0f,
+						100f
+					);
+
+					if (next != curr)
+						desires[key] = next;
+				}
+			}
+			GUILayout.EndVertical();
+		}
+
+		void Draw_Content_Sliders()
+		{
+			GUILayout.BeginVertical(GUILayout.Width(ENTRY_WIDTH));
+			{
+				Draw_Sliders_Stats();
+				Draw_Sliders_Flavors();
+			}
+			GUILayout.EndVertical();
+
+			Draw_Sliders_Desires();
+		}
+
+		void Draw_Content_Info()
+		{
+			if (controller.agent == null)
+				return;
+
+			GUILayout.BeginVertical();
+			{
+				AgentActor agent = controller.agent;
+				PlayerActor player = Manager.Map.Instance.Player;
+				Vector3 pos = controller.agent.Position;
+
+				GUILayout.Label($"Location: {(int)pos.x}, {(int)pos.y}, {(int)pos.z}");
+				GUILayout.Label($"Distance to Player: {(int)Vector3.Distance(player.Position, pos)}");
+				GUILayout.Label($"Speed: {agent.NavMeshAgent.speed}");
+				GUILayout.Label($"Phase: {agent.ChaControl.fileGameInfo.phase + 1} heart(s)");
+				GUILayout.Label($"State: {agent.StateType.ToString()}");
+
+				Sickness sick = agent.AgentData.SickState;
+				string meds = sick.UsedMedicine ? "; Medicated" : "";
+				double duration = sick.Duration.TotalSeconds;
+				string time = duration > 0 ? $"; {duration.ToString()}s" : "";
+
+				GUILayout.Label($"Sickness: {sick.Name}{time}{meds}");
+			}
+			GUILayout.EndVertical();
+		}
+
+		void Draw_Entries_Content()
+		{
+			switch (tab)
+			{
+				case 0:
+					Draw_Content_Info();
+					break;
+
+				case 1:
+					Draw_Content_Sliders();
+					break;
+			}
+		}
+
+		void Draw_Entries()
+		{
+			if (controller == null)
+				return;
+
+			Draw_Entries_Content();
+		}
+
+		void Draw_Tabs()
+		{
+			if (controller == null)
+				return;
+
+			GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+			{
+				GUILayout.Label("Tabs", labelStyle);
+
+				for (int i = 0; i < tabs.Count; i++)
+					if (GUILayout.Button(
+						tabs[i],
+						i == tab ?
+							selectedButtonStyle :
+							GUI.skin.button
+						))
+					{
+						tab = i;
+						break;
+					}
+			}
+			GUILayout.EndVertical();
+		}
+
+		void Draw_Agents()
+		{
+			GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+			{
+				GUILayout.Label("Agents", labelStyle);
+
+				foreach (StatsController controller in controllers)
+				{
+					if (controller.ChaControl == null)
+					{
+						if (this.controller == controller)
+							this.controller = null;
+
+						controllersDump.Add(controller);
+						continue;
+					}
+
+					string label;
+
+					if (CustomBase.IsInstance() &&
+						CustomBase.Instance.chaCtrl == controller.ChaControl)
+						label = "[New Agent]";
+					else if (controller.agent != null)
+						label = $"[{controller.id}]: {controller.agent.CharaName}";
+					else
+						label = $"[?]: {controller.ChaControl.chaFile.charaFileName}";
+
+					GUIStyle style =
+						this.controller == controller ? selectedButtonStyle : GUI.skin.button;
+
+					if (GUILayout.Button(label, style, GUILayout.Width(AGENTS_WIDTH)))
+						this.controller = controller;
+				}
+			}
+			GUILayout.EndVertical();
+		}
+
+		void Draw(int id)
+		{
+			GUI.DragWindow(dragRect);
+			GUILayout.BeginArea(innerRect);
+			{
+				GUILayout.BeginHorizontal();
+				{
+					GUILayout.BeginVertical(GUILayout.Width(AGENTS_WIDTH));
+					{
+						Draw_Agents();
+						Draw_Tabs();
+					}
+					GUILayout.EndVertical();
+
+					Draw_Entries();
+				}
+				GUILayout.EndHorizontal();
+			}
+			GUILayout.EndArea();
+		}
+	}
+}
---
+++ StatsController.cs
@@ -0,0 +1,123 @@
+﻿using AIProject;
+using CharaCustom;
+using ExtensibleSaveFormat;
+using KKAPI;
+using KKAPI.Chara;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AgentTrainer
+{
+	public class StatsController : CharaCustomFunctionController
+	{
+		public int id;
+		public AgentActor agent;
+
+		public Dictionary<int, float> lockedStats = new Dictionary<int, float>();
+		public Dictionary<int, float> lockedDesires = new Dictionary<int, float>();
+		public Dictionary<int, int> lockedFlavors = new Dictionary<int, int>();
+
+		protected override void OnCardBeingSaved(GameMode currentGameMode)
+		{
+			if (lockedStats.Count == 0 &&
+				lockedDesires.Count == 0 &&
+				lockedFlavors.Count == 0)
+			{
+				SetExtendedData(null);
+				return;
+			}
+
+			PluginData data = new PluginData
+			{
+				version = 1
+			};
+
+			data.data.Add("lockedStats", LZ4MessagePackSerializer.Serialize(lockedStats));
+			data.data.Add("lockedDesires", LZ4MessagePackSerializer.Serialize(lockedDesires));
+			data.data.Add("lockedFlavors", LZ4MessagePackSerializer.Serialize(lockedFlavors));
+
+			SetExtendedData(data);
+		}
+
+		protected override void OnReload(GameMode currentGameMode, bool maintainState)
+		{
+			if (CustomBase.IsInstance() &&
+				CustomBase.Instance.chaCtrl == ChaControl)
+			{
+				CustomCharaWindow[] customCharaWindows =
+					FindObjectsOfType<CustomCharaWindow>();
+
+				if (customCharaWindows != null && customCharaWindows.Length >= 2)
+				{
+					var toggles = new HarmonyLib.Traverse(customCharaWindows[1])
+						.Field("tglLoadOption")
+						.GetValue<Toggle[]>();
+
+					if (toggles != null &&
+						toggles.Length >= 5 &&
+						!toggles[4].isOn)
+						return;
+				}
+			}
+
+			PluginData data = GetExtendedData();
+
+			if (data != null)
+			{
+				Dictionary<int, float> newLockedStats;
+				Dictionary<int, float> newLockedDesires;
+				Dictionary<int, int> newLockedFlavors;
+
+				try
+				{
+					newLockedStats = LZ4MessagePackSerializer
+						.Deserialize< Dictionary<int, float>>((byte[])data.data["lockedStats"]);
+					newLockedDesires = LZ4MessagePackSerializer
+						.Deserialize<Dictionary<int, float>>((byte[])data.data["lockedDesires"]);
+					newLockedFlavors = LZ4MessagePackSerializer
+						.Deserialize<Dictionary<int, int>>((byte[])data.data["lockedFlavors"]);
+				}
+				catch (Exception err)
+				{
+					Debug.Log($"[Agent Trainer] Failed to load extended data.\n{err}");
+					return;
+				}
+
+				lockedStats = newLockedStats;
+				lockedDesires = newLockedDesires;
+				lockedFlavors = newLockedFlavors;
+			}
+		}
+
+		protected override void Start()
+		{
+			base.Start();
+			AgentTrainer.AddController(this);
+		}
+
+		void LateUpdate()
+		{
+			if (agent == null)
+				return;
+
+			Dictionary<int, float> stats = agent.AgentData.StatsTable;
+
+			foreach (KeyValuePair<int, float> stat in lockedStats)
+				stats[stat.Key] = stat.Value;
+
+			Dictionary<int, float> desires = agent.AgentData.DesireTable;
+
+			foreach (KeyValuePair<int, float> desire in lockedDesires)
+				desires[desire.Key] = desire.Value;
+
+			Dictionary<int, int> flavors = ChaControl.fileGameInfo.flavorState;
+
+			foreach (KeyValuePair<int, int> flavor in lockedFlavors)
+				if (flavors[flavor.Key] != flavor.Value)
+					agent.SetFlavorSkill(flavor.Key, flavor.Value);
+		}
+	}
+}
